c116f89e75ce0df644f33a439a1c1c1a239c17f9
APIVerve Team <hello@apiverve.com>
2024-08-20 10:48:38 -0500

    Bug Fixes

---
+++ CurrencySymbolsQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class CurrencySymbolsQueryOptions {
+public string currency { get; set; }
+}
+
+}
